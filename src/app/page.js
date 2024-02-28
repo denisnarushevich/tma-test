23c3e990041445dceed84c23ec740abd6dedaf33
Denis Narushevich <d.narushevich@gmail.com>
2024-02-28 23:01:24 +0200

    test height

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
     }, []);
 
     return (
-        <main className="flex flex-col items-center justify-between p-24">
+        <main className="flex flex-col items-center justify-between p-24  bg-gradient-to-b from-cyan-100 to-cyan-950 h-[2000px]">
             {value}
             <button onClick={handleClick} className="ring-offset-1 ring-amber-500 block">Click me!</button>
         </main>
